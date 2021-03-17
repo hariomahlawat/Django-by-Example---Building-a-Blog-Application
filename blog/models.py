206e62069f33be3f01c23a93a842942f3f700647
hariomahlawat <hariomahlawat@gmail.com>
2021-03-17 11:59:02 +0530

    list view, details view and pagination

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset()\
+        .filter(status='published')
 
 class Post(models.Model):
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
+
     STATUS_CHOICES = (
     ('draft', 'Draft'),
     ('published', 'Published'),
@@ -42,6 +51,12 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
 
     choices=STATUS_CHOICES, default='draft')
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+            args=[self.publish.year,
+                self.publish.month,
+                self.publish.day, self.slug])
 
     '''
     The Meta class inside the model contains metadata. You tell Django to sort results
